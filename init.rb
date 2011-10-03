8df3429631326cd41156ffb7fc929de431a24940
Alexandre de Oliveira <chavedomundo@gmail.com>
2011-10-03 19:22:40 -0300

    Initial plugin commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_source_control do
+  name 'Redmine Source Control plugin'
+  author 'Alexandre de Oliveira'
+  description 'Este é um plugin para catalogar códigos-fonte de projetos.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://twitter.com/kurko'
+end
